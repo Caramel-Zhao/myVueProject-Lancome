6a194dc555b8dad665120e3e9096a63d77364a8b
Caramel <34457500@qq.com>
2019-09-30 08:52:10 +0800

    1.0v

---
+++ src/components/Header/index.vue
@@ -0,0 +1,567 @@
+<template>
+  <div>
+    <header>
+      <div class="header-con">
+        <div class="header-logo clearfix">
+          <div class="logo">
+            <a href="#">
+              <img src="../../assets/logo@2x.png"
+                   alt=""
+                   class="">
+            </a>
+          </div>
+        </div>
+        <div class="header-nav clearfix">
+          <div class="nav-logo">
+            <a href="#">
+              <img src="../../assets/logo@2x.png"
+                   alt="">
+            </a>
+          </div>
+          <div class="nav-wrap">
+            <ul>
+              <li><a href="#"><span>首页</span></a></li>
+              <li class="sub-menu-btn">
+                <a href="#"><span>护肤</span></a>
+                <!-- 二级菜单 -->
+                <div class="sub-menu sub-menu-2">
+                  <div class="nav-menu">
+                    <ul>
+                      <li>
+                        <a href="#"><span>产品类型</span></a>
+                        <div class="sub-menu-3">
+                          <ul>
+                            <li><a href="#"><span>卸妆&洁面</span></a></li>
+                            <li><a href="#"><span>美容液&爽肤水</span></a></li>
+                            <li><a href="#"><span>精华</span></a></li>
+                            <li><a href="#"><span>眼部护理</span></a></li>
+                            <li><a href="#"><span>乳液</span></a></li>
+                            <li><a href="#"><span>面霜</span></a></li>
+                            <li><a href="#"><span>面膜</span></a></li>
+                            <li><a href="#"><span>防晒隔离</span></a></li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <a href="#"><span>产品系列</span></a>
+                        <div class="sub-menu-3">
+                          <ul>
+                            <li><a href="#"><span>清滢清洁系列</span></a></li>
+                            <li><a href="#"><span>小黑瓶系列</span></a></li>
+                            <li><a href="#"><span>水分缘系列</span></a></li>
+                            <li><a href="#"><span>臻白系列</span></a></li>
+                            <li><a href="#"><span>轻透防晒系列</span></a></li>
+                            <li><a href="#"><span>塑颜系列</span></a></li>
+                            <li><a href="#"><span>菁纯系列</span></a></li>
+                            <li><a href="#"><span>男士系列</span></a></li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <a href="#"><span>肌肤需求</span></a>
+                        <div class="sub-menu-3">
+                          <ul>
+                            <li><a href="#"><span>卸妆清洁</span></a></li>
+                            <li><a href="#"><span>舒缓保湿</span></a></li>
+                            <li><a href="#"><span>精华修护</span></a></li>
+                            <li><a href="#"><span>美白淡斑</span></a></li>
+                            <li><a href="#"><span>紧致提拉</span></a></li>
+                            <li><a href="#"><span>抗老愈颜</span></a></li>
+                          </ul>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="nav-banner">
+                    <div>
+                      <a href="#">
+                        <img src="../../assets/15632601677493040.jpg"
+                             alt="">
+                        <div>全新第二代小黑瓶精华</div>
+                        <p> 一瓶重现细嫩好肤质 ></p>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li class="sub-menu-btn">
+                <a href="#"><span>彩妆</span></a>
+                <!-- 二级菜单 -->
+                <div class="sub-menu sub-menu-2">
+                  <div class="nav-menu">
+                    <ul>
+                      <li>
+                        <a href="#"><span>底妆</span></a>
+                        <div class="sub-menu-3">
+                          <ul>
+                            <li><a href="#"><span>妆前乳</span></a></li>
+                            <li><a href="#"><span>气垫霜</span></a></li>
+                            <li><a href="#"><span>粉底</span></a></li>
+                            <li><a href="#"><span>蜜粉&散粉</span></a></li>
+                            <li><a href="#"><span>修容遮瑕</span></a></li>
+                            <li><a href="#"><span>腮红</span></a></li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <a href="#"><span>唇妆</span></a>
+                        <div class="sub-menu-3">
+                          <ul>
+                            <li><a href="#"><span>唇膏</span></a></li>
+                            <li><a href="#"><span>唇釉</span></a></li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <a href="#"><span>眼妆</span></a>
+                        <div class="sub-menu-3">
+                          <ul>
+                            <li><a href="#"><span>睫毛膏</span></a></li>
+                            <li><a href="#"><span>眼影</span></a></li>
+                            <li><a href="#"><span>眼线笔</span></a></li>
+                            <li><a href="#"><span>眉部</span></a></li>
+                          </ul>
+                        </div>
+                      </li>
+                      <li>
+                        <a href="#"><span>美妆&工具</span></a>
+                        <div class="sub-menu-3">
+                          <ul>
+                            <li><a href="#"><span>粉底刷</span></a></li>
+                          </ul>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="nav-banner">
+                    <div>
+                      <a href="#">
+                        <img src="../../assets/15616019720661105.jpg"
+                             alt="">
+                        <div>菁纯唇膏#196</div>
+                        <p>一抹显色 本色当红 ></p>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li class="sub-menu-btn">
+                <a href="#"><span>香水</span></a>
+                <!-- 二级菜单 -->
+                <div class="sub-menu sub-menu-2">
+                  <div class="nav-menu">
+                    <ul>
+                      <li>
+                        <a href="#"><span>女士香水</span></a>
+                        <div class="sub-menu-3">
+                          <ul>
+                            <li><a href="#"><span>美丽人生系列</span></a></li>
+                            <li><a href="#"><span>奇迹系列</span></a></li>
+                            <li><a href="#"><span>珍爱系列</span></a></li>
+                            <li><a href="#"><span>殿堂系列</span></a></li>
+                          </ul>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="nav-banner">
+                    <div>
+                      <a href="#">
+                        <img src="../../assets/15616019877279539.jpg"
+                             alt="">
+                        <div>美丽人生香水</div>
+                        <p>美丽因你而生 ></p>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li><a href="#"><span>小黑家族<span class="tag">HOT</span></span></a></li>
+              <li><a href="#"><span>热荐底妆<span class="tag">HOT</span></span></a></li>
+              <li class="sub-menu-btn">
+                <a href="#"><span>畅销榜单</span></a>
+                <div class="sub-menu sub-menu-img">
+                  <div class="aa">
+                    <img src=""
+                         alt="">
+                    <a href="#"
+                       class="a_01"></a>
+                    <a href="#"
+                       class="a_02"></a>
+                    <a href="#"
+                       class="a_03"></a>
+                    <a href="#"
+                       class="a_04"></a>
+                    <a href="#"
+                       class="a_05"></a>
+                    <a href="#"
+                       class="a_06"></a>
+                    <a href="#"
+                       class="a_07"></a>
+                  </div>
+                </div>
+              </li>
+              <li class="sub-menu-btn">
+                <a href="#"><span>官网尊享</span></a>
+                <div class="sub-menu sub-menu-img">
+                  <div class="aa">
+                    <img src="../../assets/1559786691928905.jpg"
+                         alt="">
+                  </div>
+                </div>
+              </li>
+              <li><a href="#"><span>会员中心</span></a></li>
+            </ul>
+          </div>
+          <div class="nav-right">
+            <ul>
+              <li><a href="#"><i class="icon icon-header-white"></i></a></li>
+              <li><a href="#"><i class="icon icon-header-cart"></i></a></li>
+              <li><a href="#"><i class="icon icon-header-login"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    navBind () {
+      // 首页nav鼠标移入事件
+      $(".sub-menu-btn").mouseenter(function () {
+        $(this).children(1).css({ display: "flex" }).stop().animate({
+          opacity: 1
+        }).end().children("a").addClass("change");
+      }).mouseleave(function () {
+        $(".sub-menu").css({ display: "none" }).stop().animate({
+          opacity: 0
+        }).prev().removeClass("change");
+      });
+
+      $(".nav-wrap").children(0).children("li").children("a").mouseenter(function () {
+        $(this).css({ fontWeight: 600 });
+      }).mouseleave(function () {
+        $(this).css({ fontWeight: "normal" });
+      });
+      //吸顶动画
+      $(window).scroll(function () {
+        if ($(this).scrollTop() >= $(".header-logo").height()) {
+          $("header").css({
+            position: "sticky",
+            top: $(".header-logo").height() * -1
+          });
+          $(".nav-logo").css({
+            display: "block",
+            height: "inherit",
+            float: "left",
+            position: "relative",
+            top: "12px",
+            marginRight: "29px"
+          }).stop().animate({
+            width: 112
+          }, 200);
+          $(".nav-right").stop().animate({
+            right: 29
+          }, 150);
+        }
+        if ($(this).scrollTop() <= $(".header-logo").height() || $(this).scrollTop() == 0) {
+          $(".nav-logo").css({ display: "none" }).stop().animate({
+            width: 0
+          }, 0);
+          $(".nav-right").stop().animate({
+            right: 90
+          }, 0);
+        }
+      });
+    }
+  }
+}
+</script>
+
+<style>
+header {
+  background-color: #000;
+  left: 0;
+  width: 100%;
+  z-index: 5;
+}
+
+/* loge */
+.header-logo {
+  text-align: center;
+  min-height: 78px;
+  width: 91%;
+  margin: 0 auto;
+}
+.header-logo .logo {
+  width: 153px;
+  display: inline-block;
+  padding: 32px 0 0;
+  position: absolute;
+  left: 50%;
+  margin-left: -76px;
+}
+.header-logo .logo a {
+  cursor: pointer;
+}
+
+/* nav */
+.nav-logo {
+  width: 0;
+  height: 0;
+  overflow: hidden;
+}
+.nav-logo img {
+  width: 100%;
+}
+.header-nav {
+  margin: 10px auto 0;
+  height: 50px;
+  padding: 0 78px 0 49px;
+  position: relative;
+}
+.nav-wrap {
+  margin-left: 15px;
+  width: 100%;
+}
+.nav-wrap > ul {
+  height: 50px;
+}
+.nav-wrap li {
+  display: inline-block;
+}
+.nav-wrap > ul > li > a {
+  display: block;
+  height: 50px;
+  padding: 0 23px;
+  line-height: 50px;
+  font-weight: normal;
+  color: #fff;
+  font-size: 14px;
+  text-decoration: none;
+  position: relative;
+}
+
+.nav-wrap > ul > .sub-menu-btn > a::before {
+  content: "";
+  width: 0;
+  height: 0;
+  overflow: hidden;
+  border-style: solid;
+  border-width: 7px;
+  border-color: #000 transparent transparent;
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -7px;
+  opacity: 0;
+}
+.nav-wrap > ul > .sub-menu-btn > a.change::before {
+  opacity: 1;
+  z-index: 100;
+}
+
+.tag {
+  background: #d51b51;
+  border-top-left-radius: 8px;
+  border-bottom-right-radius: 8px;
+  font-size: 12px;
+  -webkit-transform: scale(0.83333333, 0.83333333);
+  -moz-transform: scale(0.83333333, 0.83333333);
+  -ms-transform: scale(0.83333333, 0.83333333);
+  -o-transform: scale(0.83333333, 0.83333333);
+  transform: scale(0.83333333, 0.83333333);
+  line-height: 12px;
+  height: 15px;
+  display: inline-block;
+  padding: 1.5px 4px 0;
+  margin: -12px -3px 0 1px;
+  vertical-align: middle;
+}
+
+/* menu */
+.nav-right {
+  position: absolute;
+  top: 0;
+  right: 90px;
+}
+.nav-right li {
+  height: 48px;
+  display: inline-block;
+  padding: 8px 2px 18px;
+}
+.nav-right > ul > li:nth-of-type(2),
+.nav-right > ul > li:nth-of-type(3) {
+  margin-left: 10px;
+}
+.nav-right .icon {
+  display: inline-block;
+  background-image: url(../../assets/icon.png);
+}
+.icon-header-white {
+  background-position: -267px -126px;
+  width: 22px;
+  height: 22px;
+}
+.icon-header-cart {
+  background-position: 0 -273px;
+  width: 18px;
+  height: 22px;
+}
+.icon-header-login {
+  background-position: -296px -147px;
+  width: 23px;
+  height: 23px;
+}
+
+/* 二级菜单 menu2 */
+.sub-menu {
+  left: 0;
+  border-bottom: 1px solid #e0e0e0;
+  padding: 40px 0;
+  /* max-height: -moz-calc(100vh - 140px);
+    max-height: calc(100vh - 140px); */
+  /* overflow: auto; */
+  margin: 0 auto;
+  text-align: center;
+  position: absolute;
+  width: 100%;
+  top: 100%;
+  z-index: 10;
+  /* border: 1px solid #ddd; */
+  background-color: #fff;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: center;
+  display: none;
+  opacity: 0;
+}
+.sub-menu .nav-menu {
+  padding: 0;
+  margin: 0;
+  font-size: 0;
+  /* display: inline-block; */
+  float: left;
+  text-align: right;
+  width: auto;
+  /* background: #f00; */
+}
+.sub-menu .nav-menu a {
+  color: #000;
+}
+
+.sub-menu .nav-menu > ul > li {
+  min-width: 160px;
+  margin-right: 10px;
+  vertical-align: top;
+  text-align: left;
+}
+.sub-menu .nav-menu > ul > li > a {
+  color: #000;
+  display: inline-block;
+  line-height: 20px;
+  padding-bottom: 16px;
+  font-size: 15px;
+  font-weight: 700;
+}
+.sub-menu .nav-menu .sub-menu-3 {
+  width: 160px;
+  height: 228px;
+}
+.sub-menu .nav-menu .sub-menu-3 li {
+  display: block;
+  height: 18px;
+  margin-top: 12px;
+}
+.sub-menu .nav-menu .sub-menu-3 li a {
+  height: 18px;
+  line-height: 18px;
+  color: #333;
+  font-size: 14px;
+}
+
+.sub-menu .nav-banner {
+  /* display: inline-block; */
+  float: left;
+  width: 395px;
+  height: 276px;
+  padding-left: 94px;
+  border-left: 1px solid #ccc;
+}
+
+.sub-menu .nav-banner a div {
+  color: #333;
+  font-size: 15px;
+  line-height: 18px;
+  font-weight: 700;
+  padding: 15px 0 4px;
+}
+
+.sub-menu .nav-banner a p {
+  color: #333;
+  font-size: 14px;
+  line-height: 18px;
+}
+.sub-menu.sub-menu-img {
+  border: none;
+  padding: 0;
+}
+.sub-menu.sub-menu-img .aa {
+  position: absolute;
+  top: 0px;
+}
+
+.sub-menu.sub-menu-img .aa a {
+  display: block;
+  position: absolute;
+}
+
+/* 首页 畅销榜单 a标签定位 */
+.sub-menu.sub-menu-img .aa .a_01 {
+  top: 0;
+  left: 0;
+  width: 20%;
+  height: 100%;
+}
+.sub-menu.sub-menu-img .aa .a_02 {
+  top: 0;
+  left: 20%;
+  width: 26.66%;
+  height: 50%;
+}
+.sub-menu.sub-menu-img .aa .a_03 {
+  top: 0;
+  left: 46.66%;
+  width: 26.66%;
+  height: 50%;
+}
+.sub-menu.sub-menu-img .aa .a_04 {
+  top: 0;
+  right: 0%;
+  width: 26.66%;
+  height: 50%;
+}
+.sub-menu.sub-menu-img .aa .a_05 {
+  top: 50%;
+  left: 20%;
+  width: 26.66%;
+  height: 50%;
+}
+.sub-menu.sub-menu-img .aa .a_06 {
+  top: 50%;
+  left: 46.66%;
+  width: 26.66%;
+  height: 50%;
+}
+.sub-menu.sub-menu-img .aa .a_07 {
+  top: 50%;
+  right: 0%;
+  width: 26.66%;
+  height: 50%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header/index';
+export default {
+  components: {
+    Header
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
